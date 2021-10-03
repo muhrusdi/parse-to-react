2d2cfd87fdead7ca15944d1e80b2abe3c1a16242
muhrusdi <mhmrofficial@gmail.com>
2021-10-04 01:17:45 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+const acornParser = Parser.extend(require("acorn-jsx")())
+
+const parseToReact = (str) => {
+  function buildElement(nodeList) {
+    const elements = []
+
+    nodeList.map(node => {
+      if (node.type === "JSXElement") {
+        const { children, openingElement } = node
+        const { attributes } = openingElement
+        const attsObj = {}
+        attributes.forEach((item) => {
+          attsObj[item.name.name] = item.value.value;
+        })
+
+        const el = React.createElement(
+          openingElement.name.name,
+          { key: Math.random().toString(36).substring(7), ...attsObj },
+          children.length ? buildElement(children || []) : null
+        )
+
+        elements.push(el)
+      } else if (node.type === "JSXText") {
+        const { value } = node
+        elements.push(value)
+      }
+    })
+
+    return elements
+  }
+
+  try {
+    const astTree = acornParser.parse(str)
+    const expression = astTree.body[0].expression
+    return buildElement([expression])
+  } catch {
+    return null
+  }
+
+}
+
+export default parseToReact
---
+++ vite.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'main.js'),
+      name: 'parse-to-react',
+      fileName: (format) => `parse-to-react.${format}.js`
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React'
+        }
+      }
+    }
+  }
+})
